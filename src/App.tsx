d8054f5ebf5847dcd893950184c31d3ad36f43f0
SwedenguyLiam <liam.palmqvist@icloud.com>
2022-04-20 00:25:30 +0100

    Added Spotify embeds

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import s from './components/styles'
 import logoTriangle from './images/LogoTringle.svg';
+import Spotify from './components/Spotify'
 
 function App() {
   return (
@@ -8,14 +9,21 @@ function App() {
       
       <header className="App-header" style={s.h1Styles}>
 
-        <div className="div" id="top">
-        
-          <img src={logoTriangle} className="image" alt="logo"/>
-          <header className="header">Welcome to the website!</header>
+        <div className="topDiv" id="top">
+      
+          <header className="header">Nathan Dane</header>
         
         </div>  
 
+        <img src={logoTriangle} className="image" alt="logo"/>
+
         <p className="p">Welcome to the website!</p>
+
+        <Spotify link="https://open.spotify.com/artist/1oJ2Y10ugDuIaqrvYjTIF2?si=3KsIphnCRjejoxw-cY28uQ"/>
+
+        <span className="span1">
+          <p>Hello there!</p>
+        </span>
       
       </header>
     </div>
