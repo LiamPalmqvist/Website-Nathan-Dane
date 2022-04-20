f25b49faae035681e67fb97064bf7521dadbd095
SwedenguyLiam <liam.palmqvist@icloud.com>
2022-04-20 01:23:23 +0100

    Updated README.md

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import s from './components/styles'
 import logoTriangle from './images/LogoTringle.svg';
-import Spotify from './components/Spotify'
+import Spotify from './components/Spotify';
 
 function App() {
   return (
@@ -19,7 +19,7 @@ function App() {
 
         <p className="p">Welcome to the website!</p>
 
-        <Spotify link="https://open.spotify.com/artist/1oJ2Y10ugDuIaqrvYjTIF2?si=3KsIphnCRjejoxw-cY28uQ"/>
+        <Spotify link="https://open.spotify.com/album/4VPQEYE833Fk44YdfrVytp?si=DStvqt6-SO64vRpHqRpJCg"/>
 
         <span className="span1">
           <p>Hello there!</p>
