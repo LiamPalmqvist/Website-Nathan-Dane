cc0e2ad201f6c1a903d0c76c6144eae2ddb86d49
SwedenguyLiam <liam.palmqvist@icloud.com>
2022-04-20 22:24:13 +0100

    changed wording of sentences

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
 
           <div className="div">
 
-            <text className="paragraph">This cool dude is Nathan Dane. He is (not) the one responsible for the creation of this site. I know I'm just typing a bunch of shit in here but temporary text is what it is. Not go and listen to his songs, damnit!</text>
+            <text className="paragraph">This cool dude is Nathan Dane. He is (not) the one responsible for the creation of this site. I know I'm just typing a bunch of crap in here but temporary text is what it is. Not go and listen to his songs, damnit!</text>
             <Spotify link="https://open.spotify.com/album/4VPQEYE833Fk44YdfrVytp?si=DStvqt6-SO64vRpHqRpJCg" className="spotify"/>
 
           </div> 
@@ -42,13 +42,13 @@ function App() {
               <Spotify wide link="https://open.spotify.com/album/3Z9gXjkqsy8JGd0oOv3IHJ?si=PbidK7HJQMKA_v_19jxHWQ" className="spotify"/>
             </div>
             
-            <text className="paragraph">This cool dude is Nathan Dane. He is (not) the one responsible for the creation of this site. I know I'm just typing a bunch of shit in here but temporary text is what it is. Not go and listen to his songs, damnit!</text>
+            <text className="paragraph">This cool dude is Nathan Dane. He is (not) the one responsible for the creation of this site. I know I'm just typing a bunch of crap in here but temporary text is what it is. Not go and listen to his songs, damnit!</text>
             
           </div> 
 
           <div className="div">
 
-            <text className="paragraph">This cool dude is Nathan Dane. He is (not) the one responsible for the creation of this site. I know I'm just typing a bunch of shit in here but temporary text is what it is. Not go and listen to his songs, damnit!</text>
+            <text className="paragraph">This cool dude is Nathan Dane. He is (not) the one responsible for the creation of this site. I know I'm just typing a bunch of crap in here but temporary text is what it is. Not go and listen to his songs, damnit!</text>
             <Spotify link="https://open.spotify.com/album/1BifqOycPME7H2bs26yhzA?si=jOK0ksORSYukWw6RH_Tcrw" className="spotify"/>
 
           </div> 
