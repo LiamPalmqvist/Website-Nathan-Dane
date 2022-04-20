778825c50f8c66670a1addc4a302916bcd30ea9a
SwedenguyLiam <liam.palmqvist@icloud.com>
2022-04-20 21:22:48 +0100

    Aligned items better on top branch

--- src/App.tsx
+++ src/App.tsx
@@ -10,17 +10,16 @@ function App() {
       <header className="App-header" style={s.h1Styles}>
 
         <div className="topDiv" id="top">
-
+          
+          <img src={logoTriangle} className="logoImage" alt="logo"/>  
           <header className="header">Nathan Dane</header>
         
         </div>          
-        <img src={logoTriangle} className="logoImage" alt="logo"/>  
+        
       
         <span className="span">
           
-
           <p className="p">Welcome to the website!</p>
-
           <Spotify link="https://open.spotify.com/album/4VPQEYE833Fk44YdfrVytp?si=DStvqt6-SO64vRpHqRpJCg"/>
           <p>Hello there!</p>
 
