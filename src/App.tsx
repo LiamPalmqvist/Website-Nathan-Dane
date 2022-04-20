0719629cb7988395481c42eed7edc30773e560fa
SwedenguyLiam <liam.palmqvist@icloud.com>
2022-04-20 22:11:38 +0100

    Updated content in general site

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import s from './components/styles'
 import logoTriangle from './images/LogoTringle.svg';
 import profilePhoto from './images/profilePhoto.jpg'
 import Spotify from './components/Spotify';
+import theFall from './images/The_Fall_-_but_its_a_.jpeg'
 
 function App() {
   return (
@@ -22,15 +23,33 @@ function App() {
           
           <div className="div">
 
-            <img src={profilePhoto} alt="ProfilePhoto" className="displayImage"/>
-            <text className="paragraph">This cool dude is Nathan Dane. He is the one responsible for the creation of this site. I know I'm just typing a bunch of shit in here but temporary text is what it is. Not go and listen to his songs, damnit!</text>
+            <img src={profilePhoto} alt="ProfilePhoto" className="displayImage" id="border"/>
+            <text className="paragraph">This cool dude is Nathan Dane. He is (not) the one responsible for the creation of this site. I know I'm just typing a bunch of shit in here but temporary text is what it is. Not go and listen to his songs, damnit!</text>
           
           </div> 
 
           <div className="div">
 
-            <text className="paragraph">This cool dude is Nathan Dane. He is the one responsible for the creation of this site. I know I'm just typing a bunch of shit in here but temporary text is what it is. Not go and listen to his songs, damnit!</text>
+            <text className="paragraph">This cool dude is Nathan Dane. He is (not) the one responsible for the creation of this site. I know I'm just typing a bunch of shit in here but temporary text is what it is. Not go and listen to his songs, damnit!</text>
             <Spotify link="https://open.spotify.com/album/4VPQEYE833Fk44YdfrVytp?si=DStvqt6-SO64vRpHqRpJCg" className="spotify"/>
+
+          </div> 
+
+          <div className="div">
+
+            <div className="vertDiv">
+              <img src={theFall} alt="The Call" className="displayImage"/>
+              <Spotify wide link="https://open.spotify.com/album/3Z9gXjkqsy8JGd0oOv3IHJ?si=PbidK7HJQMKA_v_19jxHWQ" className="spotify"/>
+            </div>
+            
+            <text className="paragraph">This cool dude is Nathan Dane. He is (not) the one responsible for the creation of this site. I know I'm just typing a bunch of shit in here but temporary text is what it is. Not go and listen to his songs, damnit!</text>
+            
+          </div> 
+
+          <div className="div">
+
+            <text className="paragraph">This cool dude is Nathan Dane. He is (not) the one responsible for the creation of this site. I know I'm just typing a bunch of shit in here but temporary text is what it is. Not go and listen to his songs, damnit!</text>
+            <Spotify link="https://open.spotify.com/album/1BifqOycPME7H2bs26yhzA?si=jOK0ksORSYukWw6RH_Tcrw" className="spotify"/>
 
           </div> 
 
